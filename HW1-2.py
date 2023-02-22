7f83b2ee2a122b44bff22dc274206d535817972c
Dmitry Beliakov <hulk295@yandex.ru>
2023-02-22 18:44:39 +0300

    added test commit changes

--- HW1-2.py
+++ HW1-2.py
@@ -1,7 +1,7 @@
 # Задача 2: Найдите сумму цифр трехзначного числа
 # n = 123
 n = int(input('введите число: '))
-sum = 0
+sum = 0 #test commits changes
 while n > 0:
     sum = sum + n % 10
     n = n // 10
