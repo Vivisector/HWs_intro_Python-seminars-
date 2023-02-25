644bab64757a0bd0d22a7f39fd6aaf57400d5188
Dmitry Beliakov <hulk295@yandex.ru>
2023-02-25 18:33:32 +0300

    добавил наглядности при выводе ответа

--- HW3-16.py
+++ HW3-16.py
@@ -1,13 +1,15 @@
 #task 16
+# сколько раз встречается некоторое число FIND в массиве A
 import random
+LEN = 100
 #n = [1,2,3,4,5,5,6,7] # учебный массив
-list = [] # большой массив
-for i in range(0,1000):
-    x=random.randint(0,10)
-    list.append(x)
+mlist = [] #большой массив (заполним автоматом)
+for i in range(0,LEN):
+    x=random.randint(0,50)
+    mlist.append(x)
 
-find = 7 #что ищем
-cnt = 0
-for i in range(len(list)):
-    if list[i] == find: cnt +=1
-print(cnt)
+FIND = 22 #что ищем
+cnt = 0 #сколько раз нашли
+for i in range(len(mlist)):
+    if mlist[i] == FIND: cnt +=1
+print(f"Предложенное число \"{FIND}\" встречается списке из {len(mlist)} случайных элементов {mlist}\n {cnt} раз")
